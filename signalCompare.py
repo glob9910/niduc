7e45a1fe41ff036fa838dc0f7d2a4dc95721d935
krzyfok <117074791+krzyfok@users.noreply.github.com>
2024-04-06 13:47:36 +0200

    Add files via upload

---
+++ signalCompare.py
@@ -0,0 +1,7 @@
+from device import Device
+class signalCompare():
+	def signal_compare(Device_A, Device_B):#potem mo¿na to przerobic na porównywanie tablic z sygna³ami
+		if Device_A.signal==Device_B.signal:
+			print("SIGNALS EQUAL")
+		else :
+			print("ERROR")
