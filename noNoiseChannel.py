c4e5f9ae5372248351aff5cbcd44ae21282e2c9e
krzyfok <117074791+krzyfok@users.noreply.github.com>
2024-04-20 12:16:02 +0200

    signals array

--- noNoiseChannel.py
+++ noNoiseChannel.py
@@ -1,14 +1,17 @@
 from node import Node
 
 class NoNoiseChannel(Node):
+    
     def __init__(self):
         self.signal_length = 32
-        self.signal = [0] * self.signal_length
-        
-    def receive_signal(self, signal):
-        for i in range(self.signal_length):
-            self.signal[i] = signal[i] 
+        self.number_of_signals=2
+        self.signal = [[0] * self.signal_length]
+        self.data = self.signal *self.number_of_signals
 
+    def receive_signal(self, data):
+        for i in range(len(data)):
+            for j in range(len(data[i])):
+                self.data[i][j]=data[i][j]
 
     def send_signal(self):
-        return self.signal
+        return self.data
